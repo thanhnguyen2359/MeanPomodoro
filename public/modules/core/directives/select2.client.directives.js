274f9a77f3f88f1a23da2b7c8a3f86dd0a78dd70
unknown <thanh.nguyenkim@2359media.com.vn>
2014-06-17 22:25:18 -0700

    update ngSelect directive

--- public/modules/core/directives/select2.client.directives.js
+++ public/modules/core/directives/select2.client.directives.js
@@ -1,15 +1,24 @@
 'use strict';
 
-angular.module('core').directive('ngSelect',function(){
-	console.log('Select2 setting...');
-	return {
-		restrict : 'AE',
-		link : function(scope,element,attrs){
-			console.log('Select2 Directive...');
-			element.select2({
-				placeholder: 'Select a State',
-				allowClear: true
-			});
-		}
+angular.module('core').directive('ngSelect',[function(){
+	console.log('Select2 setting S...');
+	// var linker = function(scope,element,attrs){
+	// 	console.log('Select2 Directive...');
+	// 		element.select2({
+	// 			placeholder: 'Select a State',
+	// 			allowClear: true
+	// 		});
+	// };
+
+	// return {
+	// 	restrict : 'E',
+	// 	transclude : true,
+	// 	scope : {},
+	// 	link : linker
+	// };
+
+	return function(scope, element, attr){
+		console.log('Select2 Directive...');
+		element.select2();
 	};
-});
+}]);
